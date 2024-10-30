da4b006ec753ecf267a5e2f98be2734b0225d47c
Kyle Blackmon <KBlackmon@dolphins.com>
2024-10-30 11:40:38 -0400

    Reorganizing Project Directory.

---
+++ src/components/history-element/history-element.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-element',
+  standalone: true,
+  imports: [],
+  templateUrl: './history-element.component.html',
+  styleUrl: './history-element.component.css'
+})
+export class HistoryElementComponent {
+
+}
